9d29eb9c03a79558a7ede92b17b6f92b974c1409
hiiamt34 <d17dccn195@gmail.com>
2021-11-16 17:44:03 +0700

    v1

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+  var x = new Typed('.styles_typicalWrapper__1_Uvh', {
+  	strings: ['Tín Dụng','Tín Dụng Không Có Chủ Nợ', 'Cho Vay Không Trả Lãi', 'Giao Dịch Không Cần Tiền Mạt'],
+  	typeSpeed: 10,
+  	backSpeed: 10,
+  	smartBackspace: true, // this is a default
+  	loop: true
+  });
+                            
